b3c26ec8f56734bbe41040a84e720139e435469b
ndizihiwemike <142516647+ndizihiwemike@users.noreply.github.com>
2024-05-28 13:07:06 +0300

    commit changes

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
+// const express = require('express');
+// const path = require('path');
+// // const router = require('./routes/userRoute')
+
+// const app = express();
+
+// app.use(express.static(path.join(__dirname,'./')));
+
+// // app.use('/',router)
+
+// app.listen(3080,() =>{
+//     console.log(`working on port 3080`);
+// })
+
 const express = require('express');
 const path = require('path');
-// const router = require('./routes/userRoute')
+const router = require('./routes/userRoute')
 
 const app = express();
 
-app.use(express.static(path.join(__dirname,'./')));
+app.use(express.static(path.join(__dirname, "public")));
 
-// app.use('/',router)
+app.use('/',router)
 
-app.listen(3080,() =>{
-    console.log(`working on port 3080`);
+app.listen(3000,() =>{
+    console.log("working on port 3050");
 })
