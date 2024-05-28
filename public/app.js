7c5f629c8d80c00ffbf66a307a5975f665510d54
ndizihiwemike <142516647+ndizihiwemike@users.noreply.github.com>
2024-05-28 14:14:39 +0300

    commit

--- public/app.js
+++ public/app.js
@@ -1,32 +1,3 @@
-// const express = require('express');
-// const path = require('path');
-// // const router = require('./routes/userRoute')
-
-// const app = express();
-
-// app.use(express.static(path.join(__dirname,'./')));
-
-// // app.use('/',router)
-
-// app.listen(3080,() =>{
-//     console.log(`working on port 3080`);
-// })
-
-// const express = require('express');
-const path = require('path');
-const router = require('./routes/userRoute')
-
-// const app = express();
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use('/',router)
-
-app.listen(3000,() =>{
-    console.log("working on port 3050");
-})
-
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
